e86e2f7f52dfda9587ec43c124e7fe833f8981b5
John Walters <john.walters.92117@gmail.com>
2018-02-02 13:06:31 -0800

    published to azure

--- SuperCoolApp/ValuesController.cs
+++ SuperCoolApp/ValuesController.cs
@@ -15,7 +15,7 @@ namespace SuperCoolApp
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "Hello", "World" };
+            return new string[] { "Bro", "do", "you", "code?" };
         }
 
         
